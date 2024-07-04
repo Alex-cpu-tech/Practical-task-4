899c896c47919e2f2341002bbcec60718f873e2e
Alex <Alex@DESKTOP-50QRJV1>
2024-07-04 12:18:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/Main.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n, m, count = 0;
+
+    // Введення числа n
+    printf("Введіть натуральне число n: ");
+    scanf("%d", &n);
+
+    // Перевірка всіх можливих значень m
+    for (m = 1; m < n; m++) {
+        if (n / m == n % m) {
+            count++;
+        }
+    }
+
+    // Виведення результату
+    printf("Кількість рівних дільників числа %d: %d\n", n, count);
+
+    return 0;
+}
